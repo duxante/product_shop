2c61c21087379dbcef88d95b08d232809680da8d
Milorad <milorad.smilj@jsguru.io>
2023-04-27 18:48:19 +0200

    hashRouter

--- src/features/navbar/navbar.componet.jsx
+++ src/features/navbar/navbar.componet.jsx
@@ -9,16 +9,16 @@ const Navbar = () => {
         <div className="navbarHolder">
             <div className="navigation">
                 <div className="logoAndStuff">
-                   <Link to="/product_shop"><img src={logo1} className="logo" /></Link>
-                    <Link to="/product_shop">Home</Link>
-                    <Link to="product_shop/products">Products</Link>
-                    <Link to="product_shop/blog">Blog</Link>
-                    <Link to="product_shop/about">About</Link>
-                    <Link to="product_shop/contacts">Contacts</Link>
+                   <Link to="/"><img src={logo1} className="logo" /></Link>
+                    <Link to="/">Home</Link>
+                    <Link to="/products">Products</Link>
+                    <Link to="/blog">Blog</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contacts">Contacts</Link>
                 </div>
                 <div className="userIconAndCart">
-                    <Link to="product_shop/profile"><img src={userIconLogo} alt="user icon" className="userIcon" /></Link>
-                    <Link to="product_shop/cart"><img src={cartLogo} alt="shopping cart" className="shoppingcart" /></Link>
+                    <Link to="/profile"><img src={userIconLogo} alt="user icon" className="userIcon" /></Link>
+                    <Link to="/cart"><img src={cartLogo} alt="shopping cart" className="shoppingcart" /></Link>
                 </div>
             </div>
         </div>

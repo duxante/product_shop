5c20ee28d7ae677ba5df77b353f33d82f758a1e0
duxante <spomenko.dukic@gmail.com>
2023-04-27 18:33:08 +0200

    fixing links

--- src/features/navbar/navbar.componet.jsx
+++ src/features/navbar/navbar.componet.jsx
@@ -10,11 +10,11 @@ const Navbar = () => {
             <div className="navigation">
                 <div className="logoAndStuff">
                    <Link to="/product_shop"><img src={logo1} className="logo" /></Link>
-                    <Link to="/product_shop"><p>Home</p></Link>
-                    <Link to="product_shop/products"><p>Products</p></Link>
-                    <Link to="product_shop/blog"><p>Blog</p></Link>
-                    <Link to="product_shop/about"><p>About</p></Link>
-                    <Link to="product_shop/contacts"><p>Contacts</p></Link>
+                    <Link to="/product_shop">Home</Link>
+                    <Link to="product_shop/products">Products</Link>
+                    <Link to="product_shop/blog">Blog</Link>
+                    <Link to="product_shop/about">About</Link>
+                    <Link to="product_shop/contacts">Contacts</Link>
                 </div>
                 <div className="userIconAndCart">
                     <Link to="product_shop/profile"><img src={userIconLogo} alt="user icon" className="userIcon" /></Link>

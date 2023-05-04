3473257c687ac5e9470db7b1d21c26da8a93dc1d
duxante <spomenko.dukic@gmail.com>
2023-05-04 19:15:24 +0200

    adding prettierr

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
 import './App.css';
-import {
-  Routes,
-  Route,
-  Router,
-} from "react-router-dom";
+import { Routes, Route, Router } from 'react-router-dom';
 import './index.css';
 import Profile from './features/profile/profile.component';
 import Cart from './features/cart/cart.component';
 import Navbar from './features/navbar/navbar.componet';
 import Home from './features/home/home.component';
-import NotFound from "./features/notFound/notFound.component";
+import NotFound from './features/notFound/notFound.component';
 import CategoriesPage from './features/categories/categoriesPage.component';
 import ShopAll from './features/shop/shopAll.component';
 import BlogPage from './features/blogPage/blogPage.component';
 import BrandsPage from './features/brandsPage/brandsPage.component';
 import Footer from './features/footer/footer.component';
 
-
-
 function App() {
   return (
     <div className="App">
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/cart' element={<Cart />} />
-      <Route path='/categories' element={<CategoriesPage />} />
-      <Route path='/shop' element={<ShopAll />} />
-      <Route path='/blogPage' element={<BlogPage />} />
-      <Route path='/brandsPage' element={<BrandsPage />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
-    <Footer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/shop" element={<ShopAll />} />
+        <Route path="/blogPage" element={<BlogPage />} />
+        <Route path="/brandsPage" element={<BrandsPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }

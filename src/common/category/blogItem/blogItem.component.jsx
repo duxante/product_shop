08adceff011ac8f710bc8e3b5efae2d8b1b6eb0d
duxante <spomenko.dukic@gmail.com>
2023-05-11 19:15:40 +0200

    creating mui modalBlog

--- src/common/category/blogItem/blogItem.component.jsx
+++ src/common/category/blogItem/blogItem.component.jsx
@@ -1,16 +1,16 @@
-import "./blogItem.style.css";
+import './blogItem.style.css';
 
-const BlogItem = ({img, date, blogTitle, blogDescription}) => {
-    return(
-        <div className="blogItem">
-            <img src={img} />
-            <div className="blogItemText">
-                <p>{date}</p>
-                <h2>{blogTitle}</h2>
-                <p>{blogDescription}</p>
-            </div>
-        </div>
-    )
-}
+const BlogItem = ({ img, date, blogTitle, blogDescription, handleModal, blogData }) => {
+  return (
+    <div className="blogItem" onClick={() => handleModal(blogData)}>
+      <img src={img} />
+      <div className="blogItemText">
+        <p>{date}</p>
+        <h2>{blogTitle}</h2>
+        <p>{blogDescription}</p>
+      </div>
+    </div>
+  );
+};
 
 export default BlogItem;
---
+++ src/features/blogPage/components/modalBlog.component.jsx
@@ -0,0 +1,36 @@
+import './style/modalBlog.style.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+
+const ModalBlog = ({ modalData, isVisible, handleCloseModal }) => {
+  console.log(modalData);
+  return (
+    <div>
+      <Modal
+        open={isVisible}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className="modal">
+          <div className="modalInsideBox">
+            <p className="closeModal" onClick={handleCloseModal}>
+              X
+            </p>
+            <img className="modalImage" src={modalData.img} />
+            <div className="authorAndDate">
+              <p>Datum: {modalData.date}</p>
+              <p>Autor: {modalData.author}</p>
+            </div>
+            <h2 className="titleAndDescription">{modalData.blogTitle}</h2>
+            <p className="titleAndDescription">{modalData.blogDescription}</p>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalBlog;

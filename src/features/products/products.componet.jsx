2e442264c45948dd1a2eaacf47c60af3b7c222dd
duxante <spomenko.dukic@gmail.com>
2023-07-18 20:26:19 +0200

    logic for filtering products

--- src/features/products/products.componet.jsx
+++ src/features/products/products.componet.jsx
@@ -5,11 +5,30 @@ import Item from '../../common/item/item.component';
 import { products } from '../../data/products';
 
 const Products = ({ handleAddProductToCart }) => {
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const handleFilterByCategory = (category) => {
+    const filtered = products.filter((product) => product.category === category);
+    setFilteredProducts(filtered);
+  };
+  const handleResetFilter = () => {
+    setFilteredProducts([]);
+  };
   return (
     <div className="productsPageHolder">
       <Heading headingText="All Products" />
+      <div className="buttonsForCategories">
+        <button className="blueButton" onClick={() => handleResetFilter()}>
+          Show All
+        </button>
+        <button className="blueButton" onClick={() => handleFilterByCategory('Laptops')}>
+          Laptops
+        </button>
+        <button className="blueButton" onClick={() => handleFilterByCategory('Mobiles')}>
+          Mobiles
+        </button>
+      </div>
       <div className="productsPageNavigation">
-        {products.map((product, index) => (
+        {(filteredProducts.length > 0 ? filteredProducts : products).map((product, index) => (
           <Item
             product={product}
             handleAddProductToCart={handleAddProductToCart}
